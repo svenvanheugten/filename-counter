f1b2718e086b2999cb2834736542209843cb87ee
Sven van Heugten <svenvanheugten@home.nl>
2025-01-10 17:44:16 +0100

    Add test for passing in non-existing file

--- FilenameCounter.IntegrationTests/ProgramTests.cs
+++ FilenameCounter.IntegrationTests/ProgramTests.cs
@@ -1,4 +1,5 @@
-﻿using FluentAssertions;
+﻿using AutoFixture.Xunit2;
+using FluentAssertions;
 using ConsoleApp1;
 
 namespace FilenameCounter.IntegrationTests;
@@ -11,5 +12,13 @@ public class ProgramTests
         var act = () => Program.Main([]);
 
         act.Should().Throw<IndexOutOfRangeException>();
+    }
+
+    [Theory, AutoData] // TODO: Fix this bug.
+    public void Main_WithNonExistingFile_UnfortunatelyThrowsFileNotFoundException(string filename)
+    {
+        var act = () => Program.Main([filename]);
+
+        act.Should().Throw<FileNotFoundException>();
     }
 }
