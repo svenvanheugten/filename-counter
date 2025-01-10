ddae7a30e8c1b98bd338f07ac670bef4e2e50229
Sven van Heugten <svenvanheugten@home.nl>
2025-01-10 17:27:17 +0100

    Add Program from e-mail

--- FilenameCounter/Program.cs
+++ FilenameCounter/Program.cs
@@ -1,3 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, World!");
+using System;
+using System.IO;
+using System.Text;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static string[] args;
+        Program(string[] args)
+        {
+            Program.args = args;
+        }
+        private void Run()
+        {
+            var f = File.Open(args[0], FileMode.Open);
+            int pos = args[0].IndexOf('.');
+            string name = args[0].Substring(0, pos);
+            System.IO.StreamReader file = new System.IO.StreamReader(f);
+            string line;
+            int counter = 0;
+            while (true)
+            {
+                line = file.ReadLine();
+                if (line == null) break;
+                if (line.Contains(name))
+                    counter++;  
+            }
+            Console.WriteLine("found " + counter);
+        }
+        static void Main(string[] args)
+        {
+            Program program = new Program(args);
+            program.Run();
+        }
+    }
+}
