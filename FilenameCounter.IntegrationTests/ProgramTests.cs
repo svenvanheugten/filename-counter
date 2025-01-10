0de00c5d84747885d391b4dbc7855da83d35b2e6
Sven van Heugten <svenvanheugten@home.nl>
2025-01-10 18:37:23 +0100

    Use file scoped namespace

--- FilenameCounter.IntegrationTests/ProgramTests.cs
+++ FilenameCounter.IntegrationTests/ProgramTests.cs
@@ -1,6 +1,5 @@
 ﻿using AutoFixture.Xunit2;
 using FluentAssertions;
-using ConsoleApp1;
 
 namespace FilenameCounter.IntegrationTests;
 
--- FilenameCounter/Program.cs
+++ FilenameCounter/Program.cs
@@ -1,27 +1,23 @@
-using System;
-using System.IO;
-using System.Text;
-namespace ConsoleApp1
+namespace FilenameCounter;
+
+public static class Program
 {
-    public static class Program
+    public static int Main(string[] args)
     {
-        public static int Main(string[] args)
+        var f = File.Open(args[0], FileMode.Open);
+        int pos = args[0].IndexOf('.');
+        string name = args[0].Substring(0, pos);
+        System.IO.StreamReader file = new System.IO.StreamReader(f);
+        string line;
+        int counter = 0;
+        while (true)
         {
-            var f = File.Open(args[0], FileMode.Open);
-            int pos = args[0].IndexOf('.');
-            string name = args[0].Substring(0, pos);
-            System.IO.StreamReader file = new System.IO.StreamReader(f);
-            string line;
-            int counter = 0;
-            while (true)
-            {
-                line = file.ReadLine();
-                if (line == null) break;
-                if (line.Contains(name))
-                    counter++;  
-            }
-            Console.WriteLine("found " + counter);
-            return 0;
+            line = file.ReadLine();
+            if (line == null) break;
+            if (line.Contains(name))
+                counter++;  
         }
+        Console.WriteLine("found " + counter);
+        return 0;
     }
 }
