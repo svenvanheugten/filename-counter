893a3097dd2cfc8d535a3c537ea2b442cb440297
Sven van Heugten <svenvanheugten@home.nl>
2025-01-10 17:44:29 +0100

    Add tests for passing in existing files

--- FilenameCounter.IntegrationTests/ProgramTests.cs
+++ FilenameCounter.IntegrationTests/ProgramTests.cs
@@ -21,4 +21,26 @@ public class ProgramTests
 
         act.Should().Throw<FileNotFoundException>();
     }
+    
+    [Theory, AutoData] // TODO: Fix this bug.
+    public void Main_WithFileWithoutExtension_UnfortunatelyThrowsArgumentOutOfRangeException(string filename)
+    {
+        var path = Path.Combine(Path.GetTempPath(), filename);
+        using (File.Create(path)) { }
+
+        var act = () => Program.Main([path]);
+        
+        act.Should().Throw<ArgumentOutOfRangeException>();
+    }
+
+    [Theory, AutoData]
+    public void Main_WithFileWithExtension_ExitsSuccessfully(string filename, string extension)
+    {
+        var path = Path.Combine(Path.GetTempPath(), $"{filename}.{extension}");
+        using (File.Create(path)) { }
+
+        var act = () => Program.Main([path]);
+
+        act.Should().NotThrow();
+    }
 }
